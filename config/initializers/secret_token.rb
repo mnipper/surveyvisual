e1416b75768aaaf3cd6bd26759febe947861e136
Michael Nipper <mjn4406@gmail.com>
2013-07-09 13:11:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveyVisual::Application.config.secret_key_base = 'a80ff0ddaa58c6573688cc7cb4d4ffd73c671cfcffb5092d0aa964fd583deffce13e9ad658e603a0fb6cd50226f34c006acfa48823fae50aa1fbfe792b913ac2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SurveyVisual::Application.config.session_store :cookie_store, key: '_survey-visual_session'
