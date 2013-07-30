733445d244dba30f77544db11e8ad7d951cbc532
Michael Nipper <mjn4406@gmail.com>
2013-07-30 10:52:27 -0400

    Add complete answers and complete answer percent by response

--- app/models/result.rb
+++ app/models/result.rb
@@ -10,4 +10,12 @@ class Result
   def participant
     Participant.find_by(_id: self.participant_id)
   end
+
+  def complete_responses
+    answers.count
+  end
+
+  def percent_complete_responses
+    answers.count / Question.count.to_f
+  end
 end
